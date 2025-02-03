9585c5a22c7263260901cd8595dd85eeccd6230e
tape <90852088+termoyadernyy@users.noreply.github.com>
2025-02-03 21:42:00 +0200

    Update and rename image.py to api/image.py

--- api/image.py
+++ api/image.py
@@ -1,5 +1,10 @@
-# Discord Image Logger
-# By DeKrypt | https://github.com/dekrypted
+#      _.---._
+#  ,  /~~~ ~~~\  ,          
+#  |\| (0)|(0) |/|
+#  |, |  'V'  | ,|   
+#  |_/\ /⏟\ /\_| 
+#       \___/   
+#ｔｅｒｍｏｙａｄｅｒｎｙｙ 
 
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
